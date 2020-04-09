80289f5be2c3d43e9a3f10edd477b109789d96b3
Jaysonliu <leobest9688@gmail.com>
2020-04-09 23:12:25 +0800

    Primitives and Objects

---
+++ 03-Primitives vs. Objects/script.js
@@ -0,0 +1,44 @@
+/////////////////////////////
+// Lecture: Primitives vs objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// Objects
+var obj1 = {
+    name: "John",
+    age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+// expect 30 30
+
+// Functions
+var age = 27;
+var obj = {
+    name: "Jayson",
+    city: "Perth"
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = "Melbourne";
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+
+
+
+
+
+
